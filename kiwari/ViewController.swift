6e65dc0a5e51a424eb15ff19fcd382c5cac6eb31
Khairul Rijal <khairulrijl@gmail.com>
2020-02-26 11:05:02 +0700

    Initial Commit

---
+++ kiwari/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kiwari
+//
+//  Created by Khairul Rijal on 26/02/20.
+//  Copyright © 2020 Khairul Rijal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
